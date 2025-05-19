f7f0476342be5ad76928586fa5c2286ad4207bd1
none-ya <thisiscoding26@gmail.com>
2025-05-19 00:38:54 -0700

    Add files via upload

--- digital-portfolio/index.js
+++ digital-portfolio/index.js
@@ -1,3 +1,27 @@
-const background = document.getElementById('background');
+var header = document.getElementsByClassName('showcase-display');
 
-background.style.backgroundColor = "black";
+function fadeOutOnScroll(element) {
+  if (!element) {
+    return;
+  }
+  
+  var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+  var elementHeight = element.offsetHeight;
+  var scrollTop = document.documentElement.scrollTop;
+  
+  var opacity = 1;
+  
+  if (scrollTop > distanceToTop) {
+    opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+  }
+  
+  if (opacity >= 0) {
+    element.style.opacity = opacity;
+  }
+}
+
+function scrollHandler() {
+  fadeOutOnScroll(header);
+}
+
+window.addEventListener('scroll', scrollHandler);
