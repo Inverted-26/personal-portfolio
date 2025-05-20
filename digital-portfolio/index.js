6856fb20d6cc0711a81ec692a5a625f2c1b995b9
none-ya <thisiscoding26@gmail.com>
2025-05-20 10:54:44 -0700

    added files for css and animations
    scroll animation isnt working :(

--- digital-portfolio/index.js
+++ digital-portfolio/index.js
@@ -1,27 +1,9 @@
-var header = document.getElementsByClassName('showcase-display');
+// const showcaseDisplay = document.getElementsByClassName('showcase-display');
+// const showcaseAnimation = document.getElementsByClassName('showcase-animation');
 
-function fadeOutOnScroll(element) {
-  if (!element) {
-    return;
-  }
-  
-  var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
-  var elementHeight = element.offsetHeight;
-  var scrollTop = document.documentElement.scrollTop;
-  
-  var opacity = 1;
-  
-  if (scrollTop > distanceToTop) {
-    opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
-  }
-  
-  if (opacity >= 0) {
-    element.style.opacity = opacity;
-  }
-}
 
-function scrollHandler() {
-  fadeOutOnScroll(header);
-}
 
-window.addEventListener('scroll', scrollHandler);
+
+// showcaseDisplay.addEventListener('click', function(event) {
+//   showcaseDisplay.style.border = "blue";
+// });
